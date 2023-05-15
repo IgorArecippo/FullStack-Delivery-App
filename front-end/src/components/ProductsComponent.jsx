9eb06a46ae23ed98c32c41ba055dfb97c30023e0
Vitor M. Alves <vtr.vma@hotmail.com>
2023-05-15 18:24:20 -0300

    Fix: correção das funcionalidades de adição e remoção dos produtos no carrinho

--- front-end/src/components/ProductsComponent.jsx
+++ front-end/src/components/ProductsComponent.jsx
@@ -6,15 +6,14 @@ import { requestData } from '../services/requests';
 function Products() {
   const history = useHistory();
   const [isLoading, setIsLoading] = useState(true);
-  const [totalPrice, setTotalPrice] = useState(0);
+  // const [totalPrice, setTotalPrice] = useState(0);
 
   const {
     results,
     setResults,
     carrinho, setCarrinho,
+    totalPrice, setTotalPrice,
   } = useContext(DeliveryContext);
-  // const onze = 11;
-  // const resultsMap = results.slice(0, onze);
 
   const calculator = () => {
     let valor = 0;
@@ -26,19 +25,19 @@ function Products() {
     });
   };
 
-  const verifyLocalStorage = () => {
-    const carrinhoSaved = JSON.parse(localStorage.getItem('carrinho'));
-    const resultsSaved = JSON.parse(localStorage.getItem('results'));
-    const priceSaved = JSON.parse(localStorage.getItem('totalPrice'));
-    if (carrinhoSaved) {
-      setCarrinho(carrinhoSaved);
-      console.log('carrinho', carrinhoSaved);
-      setResults(resultsSaved);
-      console.log('results', resultsSaved);
-      setTotalPrice(priceSaved);
-      calculator();
-    }
-  };
+  // const verifyLocalStorage = () => {
+  //   const carrinhoSaved = JSON.parse(localStorage.getItem('carrinho'));
+  //   const resultsSaved = JSON.parse(localStorage.getItem('results'));
+  //   // const priceSaved = JSON.parse(localStorage.getItem('totalPrice'));
+  //   if (carrinhoSaved) {
+  //     setCarrinho(carrinhoSaved);
+  //     console.log('carrinho', carrinhoSaved);
+  //     setResults(resultsSaved);
+  //     console.log('results', resultsSaved);
+  //     // setTotalPrice(priceSaved);
+  //     calculator();
+  //   }
+  // };
 
   useEffect(() => {
     const featchAll = async () => {
@@ -48,12 +47,11 @@ function Products() {
         return prod;
       });
       setResults(quantidade);
-      verifyLocalStorage();
+      // verifyLocalStorage();
       calculator();
     };
     featchAll();
     setIsLoading(false);
-    console.log('infinito');
   }, []);
 
   const increaseQuantidade = (id, state) => {
@@ -77,7 +75,7 @@ function Products() {
       localStorage.setItem('results', JSON.stringify(results));
     } else {
       const product = results.find((p) => p.id === id);
-      product.quantidade = 1;
+      product.quantidade += 1;
       copyCarrinho.push(product);
       setCarrinho(copyCarrinho);
       localStorage.setItem('carrinho', JSON.stringify(copyCarrinho));
@@ -129,29 +127,29 @@ function Products() {
   };
 
   const handleInput = (event) => {
-    const prodId = Number(event.target.id);
-    const { value } = Number(event.target);
-    const newLittleCar = carrinho;
+    const { id, value } = event.target;
+    const quantity = Number(value);
 
-    if (value < 1) {
-      newLittleCar.filter((p) => p.id !== prodId);
-      return setCarrinho(newLittleCar);
-    }
-
-    const produtoNoCarrinho = carrinho.find((item) => Number(item.id) === prodId);
-    if (!produtoNoCarrinho) {
-      const resultado = results.find((item) => Number(item.id) === prodId);
-      newLittleCar.push(resultado);
-    }
-    newLittleCar.map((p) => {
-      if (Number(p.id) === prodId) {
-        p.quantidade = value;
+    const copyResults = results;
+    copyResults.map((p) => {
+      if (p.id === Number(id)) {
+        p.quantidade = quantity;
         return p;
       }
       return p;
     });
-    setCarrinho(newLittleCar);
-    localStorage.setItem('carrinho', JSON.stringify(newLittleCar));
+    setResults(copyResults);
+    localStorage.setItem('results', JSON.stringify(copyResults));
+    const verifyCarrinho = carrinho.find((p) => p.id === Number(id));
+    if (!verifyCarrinho) {
+      const product = copyResults.find((p) => p.id === Number(id));
+      const newCarrinho = carrinho;
+      newCarrinho.push(product);
+      setCarrinho(newCarrinho);
+      localStorage.setItem('carrinho', JSON.stringify(newCarrinho));
+    }
+    calculator();
+    console.log(copyResults);
   };
 
   const buttonCarrinho = () => {
@@ -200,6 +198,7 @@ function Products() {
                       id={ res.id }
                       data-testid={ `customer_products__input-card-quantity-${res.id}` }
                       value={ res.quantidade }
+                      min={ 0 }
                     />
                     <button
                       type="button"
@@ -219,10 +218,16 @@ function Products() {
         className="total-button"
         type="button"
         data-testid="customer_products__button-cart"
+        disabled={ carrinho.length === 0 }
         onClick={ buttonCarrinho }
       >
-        {`Ver carrinho: R$${totalPrice.toFixed(2)}`}
-
+        Ver Carrinho: R$
+        {' '}
+        <span
+          data-testid="customer_products__checkout-bottom-value"
+        >
+          { totalPrice.toFixed(2).replace(/\./, ',') }
+        </span>
       </button>
     </div>
   );
