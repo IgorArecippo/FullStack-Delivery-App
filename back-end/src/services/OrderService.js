c0dcae0fb46682dfa29a5cd6974f165f9167135a
João <joaolelles7@gmail.com>
2023-05-18 11:32:16 -0300

    Fix: Diminuindo as linhas da função createSale em OrderService

--- back-end/src/services/OrderService.js
+++ back-end/src/services/OrderService.js
@@ -4,12 +4,8 @@ const { Sale, SalesProducts } = require('../database/models');
 
 const createSale = async (data) => {
     const { userId,
-        sellerId,
-        totalPrice,
-        deliveryAddress,
-        deliveryNumber,
-        status,
-        carrinho,
+        sellerId, totalPrice, deliveryAddress, 
+        deliveryNumber, status, carrinho,
     } = data;
     const sale = await Sale.create({ userId,
         sellerId,
@@ -19,16 +15,12 @@ const createSale = async (data) => {
         saleDate: new Date(),
         status,
     });
-
     const { id } = sale;
-
     const newCarrinho = carrinho.map((p) => {
         const obj = { saleId: id, productId: p.id, quantity: p.quantidade };
         return obj;
     });
-
     await SalesProducts.bulkCreate(newCarrinho);
-
     return id;
 };
 
